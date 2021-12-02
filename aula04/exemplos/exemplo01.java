4fcecbd2390cc90dd939839961400fdc533641e9
karinasrib <karinasrib@gmail.com>
2021-12-02 11:55:50 -0300

    aula 04 - parte 1

---
+++ aula04/exemplos/exemplo01.java
@@ -0,0 +1,22 @@
+package exemplos;
+
+/**
+ * exemplo01
+ */
+public class exemplo01 {
+
+    public static void main(String[] args) {
+        double nota;
+        nota = 5;
+
+        if(nota >= 6) {
+            System.out.println("Aprovado!");
+        } else {
+            System.out.println("Reprovado... :-(");
+
+        }
+
+        
+
+    }
+}
---
+++ aula04/exemplos/exemplo02.java
@@ -0,0 +1,24 @@
+package exemplos;
+
+public class exemplo02 {
+    public static void main(String[] args) {
+
+        // printf(FORMATO, DADOS)
+        // FORMATO:
+        // %d = iinteiro
+        // %f + decimal
+        // %s = texto
+        // \n = pul de linha
+
+        // %.2f = 2 casas decimais, comm arredondamento
+
+        System.out.printf("O número é %d\n", 10);
+
+        System.out.printf("O número são %d e %d\n", 10, 15);
+
+        System.out.printf("A média é %f\n", 45.6754321);
+
+        System.out.printf("A média é %.2f\n", 45.6754321);
+
+    }
+}
