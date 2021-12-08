089c3a5eadd18aa2d0f735c6f4fc3be9402e7b39
karinasrib <karinasrib@gmail.com>
2021-12-08 15:59:33 -0300

    conta

---
+++ conta/AppContas.java
@@ -0,0 +1,26 @@
+
+
+import modelo.ContaCorrente;
+import modelo.ContaEspecial;
+import modelo.ContaPoupanca;
+
+public class AppContas {
+    public static void main(String[] args) {
+        ContaPoupanca conta = new ContaPoupanca(123);
+
+        conta.setTaxaOperacao(0.1);
+
+        System.out.println(conta);
+
+        conta.deposito(200);
+        System.out.println(conta);
+
+        if (conta.saque(100)) {
+            System.out.println("Saque realizado.");
+        } else {
+            System.out.println("Saldo insuficinete.");
+        }
+
+        System.out.println(conta);
+    }
+}
---
+++ conta/modelo/ContaCorrente.java
@@ -0,0 +1,29 @@
+package modelo;
+
+public class ContaCorrente extends Conta {
+    final double TAXA_DEPOSITO = 0.1;
+
+    public ContaCorrente(int numero) {
+        super(numero);
+    }
+
+    @Override
+    public boolean saque(double valor) {
+        if (valor <= getSaldo()) {
+            return super.saque(valor);
+        }
+        return false; // não tem saldo suficiente na conta
+    }
+
+    @Override
+    public boolean deposito(double valor) {
+        return super.deposito(valor - TAXA_DEPOSITO);
+
+    }
+
+    public String toString(){
+         return "CC: " + super.toString();
+    }
+
+
+}
